87668d08544c2348dcf6b111c4a59eeef57a1db8
Maria Eduarda Figueiredo <128522652+mariaeduardafigueiredo@users.noreply.github.com>
2024-07-17 15:43:28 -0300

    Delete trilhaBackEndJR_JUN15 directory

--- trilhaBackEndJR_JUN15/src/main/java/com/pxp/SQLite/trilhaBackEndJR_JUN15/repository/PerfilRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pxp.SQLite.trilhaBackEndJR_JUN15.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.napoleao.projeto.entity.PerfilEntity;
-
-public interface PerfilRepository extends JpaRepository<PerfilEntity, Long>{
-
-}
--- trilhaBackEndJR_JUN15/src/main/java/com/pxp/SQLite/trilhaBackEndJR_JUN15/repository/RecursoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pxp.SQLite.trilhaBackEndJR_JUN15.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.napoleao.projeto.entity.RecursoEntity;
-
-public interface RecursoRepository extends JpaRepository<RecursoEntity, Long>{
-
-}
